97282ff49e2c8169b98602f1c8df9d4d11edd32f
online5880 <gjtjqkr1@naver.com>
2023-10-10 01:12:40 +0900

    04_대소문자 바꿔서 출력하기

---
+++ 04_대소문자 바꿔서 출력하기/04_대소문자 바꿔서 출력하기.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+// 문제 설명
+// 영어 알파벳으로 이루어진 문자열 str이 주어집니다.각 알파벳을 대문자는 소문자로
+// 소문자는 대문자로 변환해서 출력하는 코드를 작성해 보세요.
+
+// 제한사항
+// 1 ≤ str의 길이 ≤ 20
+// str은 알파벳으로 이루어진 문자열입니다.
+
+int main(void) {
+    string str;
+    cin >> str;
+
+    for (const char c : str)
+    {
+        cout << (isupper(c) ? static_cast<char>(tolower(c)) : static_cast<char>(toupper(c)));
+    }
+
+    return 0;
+}
