bff6f0be2c13aab0ce7c49b3f85eb8f9a9348023
online5880 <gjtjqkr1@naver.com>
2023-10-12 01:27:13 +0900

    14_더 크게 합치기

---
+++ 14_더 크게 합치기/14_더 크게 합치기.cpp
@@ -0,0 +1,16 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b) {
+    int answer = 0;
+    string str1 = to_string(a)+ to_string(b);
+    string str2 = to_string(b)+ to_string(a);
+
+    answer = stoi(str1) > stoi(str2) ? stoi(str1) : stoi(str2);
+    return answer;
+
+    // 다른 사람 문제 풀이
+    //return max(stoi(to_string(a) + to_string(b)), stoi(to_string(b) + to_string(a)));
+}
