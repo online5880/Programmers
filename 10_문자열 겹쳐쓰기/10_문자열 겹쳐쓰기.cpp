61eb71d5ff9ade459631ca9b395d00564f8a5440
online5880 <gjtjqkr1@naver.com>
2023-10-12 00:48:32 +0900

    09_홀짝 구분하기 10_문자열 겹쳐쓰기

---
+++ 10_문자열 겹쳐쓰기/10_문자열 겹쳐쓰기.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// 내 풀이
+string solution(string my_string, string overwrite_string, int s) {
+    int index = 0;
+    for (int i = s; i < overwrite_string.size() + s; ++i)
+    {
+        my_string[i] = overwrite_string[index++];
+    }
+    string answer = my_string;
+    return answer;
+}
+
+/* 다른 사람 풀이
+string solution(string my_string, string overwrite_string, int s) {
+    string answer = "";
+    answer = my_string.replace(s, overwrite_string.size(), overwrite_string);
+    return answer;
+}
+*/
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
