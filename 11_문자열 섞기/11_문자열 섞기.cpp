10def15cc062e1367c5171d56f0696dc2863fc2c
online5880 <gjtjqkr1@naver.com>
2023-10-12 01:03:26 +0900

    12_문자 리스트를 문자열로 변환하기

--- 11_문자열 섞기/11_문자열 섞기.cpp
+++ 11_문자열 섞기/11_문자열 섞기.cpp
@@ -11,5 +11,14 @@ string solution(string str1, string str2) {
         answer += str1[i];
         answer += str2[i];
     }
+
+    // 다른 사람 풀이
+    /*
+    for (int i = 0; i < str1.length(); i++)
+    {
+        answer.push_back(str1[i]);
+        answer.push_back(str2[i]);
+    }
+    */
     return answer;
 }
---
+++ 12_문자 리스트를 문자열로 변환하기/12_문자 리스트를 문자열로 변환하기.cpp
@@ -0,0 +1,13 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> arr) {
+    string answer = "";
+    for (const string c : arr)
+    {
+        answer += c;
+    }
+    return answer;
+}
