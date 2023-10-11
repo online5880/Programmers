965931d5a026c52156b26388d3d8728dffec8660
online5880 <gjtjqkr1@naver.com>
2023-10-12 01:11:27 +0900

    13_문자열 곱하기

---
+++ 13_문자열 곱하기/13_문자열 곱하기.cpp
@@ -0,0 +1,16 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, int k) {
+    string answer = "";
+    for (int i = 0; i < k; ++i)
+    {
+        answer += my_string;
+    }
+
+    // 다른 사람 풀이
+    // while (k--) answer.append(my_string);
+    return answer;
+}
