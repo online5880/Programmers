1c9fafaddd7c325b7a7943b3c61bda175c42cf04
online5880 <gjtjqkr1@naver.com>
2023-10-12 21:14:11 +0900

    15_두 수의 연산값 비교하기 16_n의 배수

---
+++ 15_두 수의 연산값 비교하기/15_두 수의 연산값 비교하기.cpp
@@ -0,0 +1,12 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b) {
+    int answer = 0;
+    int aa = stoi(to_string(a) + to_string(b));
+    int bb = 2 * a * b;
+    answer = (aa >= bb) ? aa : bb;
+    return answer;
+}
