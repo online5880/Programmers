13b6bf5682f09ce3ac117cac3e3a4f4272fb50c6
online5880 <gjtjqkr1@naver.com>
2023-10-13 09:07:43 +0900

    19_조건 문자열

---
+++ 19_조건 문자열/19_조건 문자열.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+int solution(std::string ineq, std::string eq, int n, int m) {
+    int answer = 0;
+    char c[2];
+    strcpy(c, (ineq + eq).c_str());
+    if (strcmp(c, ">=") == 0)
+    {
+        answer = (n >= m);
+    }
+    else if (strcmp(c, "<=") == 0)
+    {
+        answer = (n <= m);
+    }
+    else if (strcmp(c, ">!") == 0)
+    {
+        answer = (n > m);
+    }
+    else
+    {
+        answer = (n < m);
+    }
+    return answer;
+}
+// 다른 사람 풀이
+//int solution(string ineq, string eq, int n, int m) {
+//    string oper = ineq + eq;
+//    if (oper == "<=")
+//    {
+//        return n <= m;
+//    }
+//    else if (oper == ">=")
+//    {
+//        return n >= m;
+//    }
+//    else if (oper == "<!")
+//    {
+//        return n < m;
+//    }
+//    else if (oper == ">!")
+//    {
+//        return n > m;
+//    }
+//}
