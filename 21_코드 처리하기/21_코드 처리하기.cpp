a80bf133c746a66c4125c1e5fc6df67ab0243e8f
online5880 <gjtjqkr1@naver.com>
2023-10-16 02:00:14 +0900

    21_코드 처리하기

---
+++ 21_코드 처리하기/21_코드 처리하기.cpp
@@ -0,0 +1,41 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string code) {
+    string answer = "";
+    bool mode = false;
+    for (int i = 0; i < code.length(); ++i)
+    {
+        if (!mode)
+        {
+            if (code[i] != '1' && i % 2 == 0)
+            {
+                answer += code[i];
+            }
+            else if (code[i] == '1')
+            {
+                mode = true;
+            }
+        }
+        else
+        {
+            if (code[i] != '1' && i % 2 == 1)
+            {
+                answer += code[i];
+            }
+            else if (code[i] == '1')
+            {
+                mode = false;
+            }
+        }
+    }
+
+    if (answer.empty())
+    {
+        answer = "EMPTY";
+    }
+
+    return answer;
+}
