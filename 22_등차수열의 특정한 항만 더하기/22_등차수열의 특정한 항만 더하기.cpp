e14ba60133ef691f3fc94cc399b3a088a8c3bcb4
online5880 <gjtjqkr1@naver.com>
2023-10-16 23:51:55 +0900

    22_등차수열의 특정한 항만 더하기

---
+++ 22_등차수열의 특정한 항만 더하기/22_등차수열의 특정한 항만 더하기.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int d, vector<bool> included) {
+    int answer = 0;
+    for (int i = a; i < included.size(); i = a + d)
+    {
+        if (included[i] == true)
+        {
+            answer += i;
+        }
+    }
+    for (auto it = included.begin(); it != included.end(); ++it)
+    {
+	    
+    }
+
+    return answer;
+
+    
+};
