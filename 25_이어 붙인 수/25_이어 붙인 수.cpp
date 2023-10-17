c5ccd01f586340602931d66fdc4cccbc753c1a59
online5880 <gjtjqkr1@naver.com>
2023-10-18 00:18:57 +0900

    25_이어 붙인 수

---
+++ 25_이어 붙인 수/25_이어 붙인 수.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = 0;
+    string even;
+    string odd;
+    for (const int n : num_list)
+    {
+        if (n % 2 == 0)
+        {
+            even += (to_string(n));
+        }
+        else
+        {
+            odd += (to_string(n));
+        }
+    }
+    answer = stoi(even) + stoi(odd);
+    return answer;
+}
