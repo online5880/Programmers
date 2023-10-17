148636aa3b8b1a793412da1a27dab718e87afe4a
online5880 <gjtjqkr1@naver.com>
2023-10-18 00:13:18 +0900

    24_원소들의곱과 합

---
+++ 24_원소들의 곱과 합/24_원소들의 곱과 합.cpp
@@ -0,0 +1,16 @@
+﻿#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = 0;
+    int a = 1; // 모든 원소의 곱
+    int b = 0; // 합의 제곱
+    for (const int& n : num_list)
+    {
+        a *= n;
+        b += n;
+    }
+    answer = (a < pow(b, 2));
+    return answer;
+}
