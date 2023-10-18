9e150c9e1b4df53c533663afc60dd194454b3622
online5880 <gjtjqkr1@naver.com>
2023-10-19 01:09:11 +0900

    27_수 조각하기 1

---
+++ 27_수 조작하기 1/27_수 조작하기 1.cpp
@@ -0,0 +1,29 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, string control) {
+    int answer = n;
+    for (const char c : control)
+    {
+        if (c == 'w')
+        {
+            answer += 1;
+        }
+        if (c == 's')
+        {
+            answer -= 1;
+        }
+        if (c == 'd')
+        {
+            answer += 10;
+        }
+        if (c == 'a')
+        {
+            answer -= 10;
+        }
+    }
+
+    return answer;
+}
