863ec197262aeff31bd2da11fe7425e2b1192d69
online5880 <gjtjqkr1@naver.com>
2023-10-19 01:08:18 +0900

    26_마지막 두 원소

---
+++ 26_마지막 두 원소/26_마지막 두 원소.cpp
@@ -0,0 +1,22 @@
+﻿#include <vector>
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    vector<int> answer;
+    for (int i = 0; i < num_list.size(); ++i)
+    {
+        answer.emplace_back(num_list[i]);
+        if (i == num_list.size() - 1)
+        {
+            if (num_list[i] > num_list[i - 1])
+            {
+                answer.emplace_back(num_list[i] - num_list[i - 1]);
+            }
+            else
+            {
+                answer.emplace_back(num_list[i] * 2);
+            }
+        }
+    }
+    return answer;
+}
