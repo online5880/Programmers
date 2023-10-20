7b1b33b5891ba2e151c60bd218a79eab28227f17
online5880 <gjtjqkr1@naver.com>
2023-10-20 22:56:35 +0900

    29_수열과 구간 쿼리 3

---
+++ 29_수열과 구간 쿼리 3/29_수열과 구간 쿼리 3.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
+    vector<int> answer = arr;
+    for (int i = 0; i < queries.size(); ++i)
+    {
+        int tmp = answer[queries[i][0]];
+        answer[queries[i][0]] = answer[queries[i][1]];
+        answer[queries[i][1]] = tmp;
+    }
+    return answer;
+}
+
+// 다른 사람 풀이
+/*
+vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
+
+    for (const auto& q : queries)
+    {
+        swap(arr[q[0]], arr[q[1]]);
+    }
+    return arr;
+}
+*/
