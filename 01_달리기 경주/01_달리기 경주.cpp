f8c470e36f716b654878d56aa6d7887534e55924
online5880 <gjtjqkr1@naver.com>
2023-10-23 23:53:59 +0900

    01_달리기 경주

---
+++ 01_달리기 경주/01_달리기 경주.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+vector<string> solution(vector<string> players, vector<string> callings) {
+
+    unordered_map<string, int> result;
+
+    for (int i = 0; i < players.size(); ++i)
+    {
+        result[players[i]] = i;
+        cout << result[players[i]];
+    }
+
+    for (const string& calling : callings)
+    {
+        int currentPosition = result[calling];
+        if (currentPosition > 0)
+        {
+            swap(players[currentPosition], players[currentPosition - 1]);
+            result[calling] = currentPosition - 1;
+            result[players[currentPosition]] = currentPosition;
+        }
+    }
+    return players;
+}
