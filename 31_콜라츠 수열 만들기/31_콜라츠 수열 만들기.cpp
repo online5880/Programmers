dd7a1221fff26f35906b11c2499f713b246140c7
online5880 <gjtjqkr1@naver.com>
2023-10-24 22:08:41 +0900

    31_콜라츠 수열 만들기

---
+++ 31_콜라츠 수열 만들기/31_콜라츠 수열 만들기.cpp
@@ -0,0 +1,21 @@
+﻿#include <vector>
+using namespace std;
+
+vector<int> solution(int n) {
+    vector<int> answer;
+    answer.emplace_back(n);
+    while (n != 1)
+    {
+        if (n & 1)
+        {
+            n = (n * 3) + 1;
+            answer.emplace_back(n);
+        }
+        else
+        {
+            n = (n /= 2);
+            answer.emplace_back(n);
+        }
+    }
+    return answer;
+}
