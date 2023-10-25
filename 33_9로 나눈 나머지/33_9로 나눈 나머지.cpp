82ec568835e47582ba60e4ed0789f1a8cc2e2a40
online5880 <gjtjqkr1@naver.com>
2023-10-26 00:15:02 +0900

    9로 나눈 나머지

---
+++ 33_9로 나눈 나머지/33_9로 나눈 나머지.cpp
@@ -0,0 +1,11 @@
+﻿#include <string>
+using namespace std;
+
+int solution(string number) {
+    int answer = 0;
+    for (const char i : number)
+    {
+        answer += (i - 48);
+    }
+    return answer % 9;
+}
