ae47cb051561529d414bdb3b78b7f0853bd37461
online5880 <gjtjqkr1@naver.com>
2023-10-25 20:39:00 +0900

    32_글자 이어 붙여 문자열 만들기

---
+++ 32_글자 이어 붙여 문자열 만들기/32_글자 이어 붙여 문자열 만들기.cpp
@@ -0,0 +1,13 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, vector<int> index_list) {
+    string answer = "";
+    for (int n : index_list)
+    {
+        answer += my_string[n];
+    }
+    return answer;
+}
