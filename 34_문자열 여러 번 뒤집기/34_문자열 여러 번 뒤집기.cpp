a01f10d510f0f97eac6ac02c76bb86fadf1416db
online5880 <gjtjqkr1@naver.com>
2023-10-26 00:30:49 +0900

    문자열 여러 번 뒤집기

---
+++ 34_문자열 여러 번 뒤집기/34_문자열 여러 번 뒤집기.cpp
@@ -0,0 +1,13 @@
+﻿#include <string>
+#include <vector>
+using namespace std;
+
+string solution(string my_string, vector<vector<int>> queries) {
+    string answer = "";
+    for (vector<int> n : queries)
+    {
+        reverse(my_string.begin() + n[0], my_string.begin() + n[1] + 1);
+    }
+    answer = my_string;
+    return answer;
+}
