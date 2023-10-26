3e513af31ba57abf53408f393fea7a753cf04184
online5880 <gjtjqkr1@naver.com>
2023-10-27 01:03:02 +0900

    35_수열과 구간 쿼리 2 36_배열 만들기 4

---
+++ 35_수열과 구간 쿼리 2/35_수열과 구간 쿼리 2.cpp
@@ -0,0 +1,22 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
+    vector<int> answer;
+    for (vector<int>& query : queries)
+    {
+        int value = 10000001;
+        for (int i = query[0]; i <= query[1]; ++i)
+        {
+            if (arr[i] > query[2] && arr[i] < value)
+            {
+                value = arr[i];
+            }
+        }
+        if (value == 10000001) value = -1;
+        answer.emplace_back(value);
+    }
+    return answer;
+}
---
+++ 36_배열 만들기 4/36_배열 만들기 4.cpp
@@ -0,0 +1,43 @@
+﻿#include <vector>
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> stk;
+    int i = 0;
+
+    for (i; i < arr.size();)
+    {
+        if (stk.empty())
+        {
+            stk.emplace_back(arr[i]);
+            ++i;
+        }
+        else
+        {
+            if (stk[stk.size() - 1] < arr[i])
+            {
+                stk.emplace_back(arr[i]);
+                ++i;
+                continue;
+            }
+            if (stk[stk.size() - 1] >= arr[i])
+            {
+                stk.pop_back();
+            }
+        }
+    }
+    return stk;
+}
+
+/*
+vector<int> solution(vector<int> arr) {
+    vector<int> stk;
+    int i = 0;
+    while (i < arr.size()) {
+        if (stk.empty()) stk.push_back(arr[i++]);
+        else if (stk.back() < arr[i]) stk.push_back(arr[i++]);
+        else stk.erase(stk.end() - 1);
+    }
+    return stk;
+}
+*/
