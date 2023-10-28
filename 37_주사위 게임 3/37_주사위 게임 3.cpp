fa900dc3b09a6d06a24a15dc768671467d8fcb74
online5880 <gjtjqkr1@naver.com>
2023-10-29 02:05:26 +0900

    37_주사위 게임 3

---
+++ 37_주사위 게임 3/37_주사위 게임 3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+int solution(int a, int b, int c, int d) {
+    int answer = 0;
+    std::vector<int> numbers{ a,b,c,d };
+    std::vector<std::string> strings;
+    for (int n : numbers)
+    {
+        strings.emplace_back(std::to_string(n));
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
