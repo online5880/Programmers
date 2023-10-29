3514b2dd90d2183e7fd75e10c5bb68d443ae2698
online5880 <gjtjqkr1@naver.com>
2023-10-30 00:24:00 +0900

    39_부분 문자열 이어 붙여 문자열 만들기

---
+++ 39_부분 문자열 이어 붙여 문자열 만들기/39_부분 문자열 이어 붙여 문자열 만들기.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> my_strings, vector<vector<int>> parts) {
+    string answer = "";
+    for (int i = 0; i < my_strings.size(); ++i)
+    {
+        for (int k = parts[i][0]; k <= parts[i][1]; ++k)
+        {
+            string temp;
+            temp += my_strings[i][k];
+            answer += temp;
+        }
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
