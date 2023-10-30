421cec8d66781474a2b80cdbb7b994233639c200
online5880 <gjtjqkr1@naver.com>
2023-10-31 00:32:01 +0900

    40_수열과 구간 쿼리 4

--- 37_주사위 게임 3/37_주사위 게임 3.cpp
+++ 37_주사위 게임 3/37_주사위 게임 3.cpp
@@ -1,16 +1,23 @@
-﻿#include <iostream>
-#include <string>
+﻿#include <algorithm>
+#include <iostream>
 #include <vector>
+using namespace std;
 
 int solution(int a, int b, int c, int d) {
-    int answer = 0;
-    std::vector<int> numbers{ a,b,c,d };
-    std::vector<std::string> strings;
-    for (int n : numbers)
+    vector<int> numbers{ a,b,c,d };
+    sort(numbers.begin(), numbers.end());
+
+    if (numbers[0] == numbers[3]) return 1111 * numbers[0];
+    else if (numbers[0] == numbers[1] && numbers[2] == numbers[3])
     {
-        strings.emplace_back(std::to_string(n));
+        return (numbers[0] + numbers[2]) * (abs(numbers[0] - numbers[2]));
     }
-    return answer;
+    else if (numbers[1] == numbers[2] && (numbers[0] == numbers[1] || numbers[2] == numbers[3]))
+    {
+        return pow(10 * numbers[1] + (numbers[0] != numbers[1] ? numbers[0] : numbers[3]), 2);
+    }
+    else if (numbers[0] != numbers[1] && numbers[1] != numbers[2] && numbers[2] != numbers[3]) return numbers[0];
+    else return numbers[0] == numbers[1] ? numbers[2] * numbers[3] : (numbers[1] == numbers[2] ? numbers[0] * numbers[3] : numbers[0] * numbers[1]);
 }
 
 int main()
---
+++ 40_수열과 구간 쿼리 4/40_수열과 구간 쿼리 4.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
+    vector<int> answer;
+    for (vector<int> query : queries)
+    {
+        for (int i = query[0]; i <= query[1]; ++i)
+        {
+            if (query[0] <= i && i <= query[1])
+            {
+                if (i % query[2] == 0)
+                {
+                    arr[i] += 1;
+                }
+            }
+        }
+    }
+    return arr;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
