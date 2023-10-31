5726a8e7a19f30a2d340b0785b618776e1330243
online5880 <gjtjqkr1@naver.com>
2023-10-31 20:27:25 +0900

    42_문자열 뒤의 n글자

---
+++ 42_문자열 뒤의  n글자/42_문자열 뒤의  n글자.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+using namespace std;
+
+string solution(string my_string, int n) {
+    string answer = "";
+    answer = my_string.substr(my_string.size() - n);
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
