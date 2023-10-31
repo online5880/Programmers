89794263ca3297e5674e0b65e5217ba3df497fde
online5880 <gjtjqkr1@naver.com>
2023-10-31 20:26:32 +0900

    41_간단한 논리 연산

---
+++ 41_간단한 논리 연산/41_간단한 논리 연산.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(bool x1, bool x2, bool x3, bool x4) {
+    return (x1 || x2) && (x3 || x4);
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
