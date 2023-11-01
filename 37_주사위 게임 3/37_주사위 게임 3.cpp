ddd83c029e7c3d3b1bd7cb37b079fad7731c56c3
online5880 <gjtjqkr1@naver.com>
2023-11-01 20:36:35 +0900

    37_주사위 게임 3 44_접미사 배열

--- 37_주사위 게임 3/37_주사위 게임 3.cpp
+++ 37_주사위 게임 3/37_주사위 게임 3.cpp
@@ -4,20 +4,40 @@
 using namespace std;
 
 int solution(int a, int b, int c, int d) {
-    vector<int> numbers{ a,b,c,d };
+    int answer = 0;
+    vector<int> numbers = { a,b,c,d };
     sort(numbers.begin(), numbers.end());
 
-    if (numbers[0] == numbers[3]) return 1111 * numbers[0];
+    if (numbers[0] == numbers[3])
+    {
+        return 1111 * numbers[0];
+    }
     else if (numbers[0] == numbers[1] && numbers[2] == numbers[3])
     {
-        return (numbers[0] + numbers[2]) * (abs(numbers[0] - numbers[2]));
+        return (numbers[0] + numbers[2]) * abs(numbers[0] - numbers[2]);
     }
     else if (numbers[1] == numbers[2] && (numbers[0] == numbers[1] || numbers[2] == numbers[3]))
     {
         return pow(10 * numbers[1] + (numbers[0] != numbers[1] ? numbers[0] : numbers[3]), 2);
     }
-    else if (numbers[0] != numbers[1] && numbers[1] != numbers[2] && numbers[2] != numbers[3]) return numbers[0];
-    else return numbers[0] == numbers[1] ? numbers[2] * numbers[3] : (numbers[1] == numbers[2] ? numbers[0] * numbers[3] : numbers[0] * numbers[1]);
+    else if (numbers[0] != numbers[1] && numbers[1] != numbers[2] && numbers[2] != numbers[3])
+    {
+        return numbers[0];
+    }
+    else if (numbers[0] == numbers[1])
+    {
+        return numbers[2] * numbers[3];
+    }
+    else if (numbers[1] == numbers[2])
+    {
+        return numbers[0] * numbers[3];
+    }
+    else if (numbers[2] == numbers[3])
+    {
+        return numbers[0] * numbers[1];
+    }
+
+    return answer;
 }
 
 int main()
---
+++ 44_접미사 배열/44_접미사 배열.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<string> solution(string my_string) {
+    vector<string> answer;
+    for (int i = 0; i < my_string.length(); ++i)
+    {
+        answer.emplace_back(my_string.substr(i));
+    }
+
+    sort(answer.begin(), answer.end());
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
