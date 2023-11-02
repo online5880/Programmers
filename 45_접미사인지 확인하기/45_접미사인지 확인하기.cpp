0504aa881da78e8495e52c5effd7e8eaa143ae1c
online5880 <gjtjqkr1@naver.com>
2023-11-02 23:53:54 +0900

    45_접미사인지 확인하기

---
+++ 45_접미사인지 확인하기/45_접미사인지 확인하기.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int solution(string my_string, string is_suffix) {
+    int answer = 0;
+
+    if (my_string.length() >= is_suffix.length())
+    {
+        if (my_string.substr(my_string.length() - is_suffix.length()) == is_suffix)
+        {
+            return true;
+        }
+    }
+
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
