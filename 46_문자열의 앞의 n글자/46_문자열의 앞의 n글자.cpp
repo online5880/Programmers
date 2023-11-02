72045e24a9f8e73769f8bb4940d584ff78668b39
online5880 <gjtjqkr1@naver.com>
2023-11-03 00:22:23 +0900

    46_문자열의 앞의 n글자

---
+++ 46_문자열의 앞의 n글자/46_문자열의 앞의 n글자.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, int n) {
+    string answer = "";
+    for (int i = 0; i < n; ++i)
+    {
+        answer += my_string.c_str()[i];
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
