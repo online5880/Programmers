856ff6235ccd372a9f7526e57be8d93dbe376ef6
online5880 <gjtjqkr1@naver.com>
2023-11-05 22:36:44 +0900

    48_세로 읽기

---
+++ 48_세로 읽기/48_세로 읽기.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+std::string solution(std::string my_string, int m, int c) {
+	std::string answer = "";
+    for (int i = 0; i < my_string.length(); ++i)
+    {
+        if (i % m + 1 == c)
+        {
+            answer += my_string[i];
+        }
+    }
+
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
---
+++ 49_qr code/49_qr code.cpp
@@ -0,0 +1,25 @@
+﻿// 49_qr code.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+string solution(int q, int r, string code) {
+    string answer = "";
+    for (int i = 0; i < code.length(); ++i)
+    {
+        if (i % q == r)
+        {
+            answer += code[i];
+        }
+
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
