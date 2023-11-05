429ad15a94eafd0de6b9363215cc83d14da41148
online5880 <gjtjqkr1@naver.com>
2023-11-06 00:04:59 +0900

    50_문자 개수 세기

---
+++ 50_문자 개수 세기/50_문자 개수 세기.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(string my_string) {
+    vector<int> answer(52, 0);
+    for (const char c : my_string)
+    {
+        if ((int)c <= (int)'Z')
+        {
+            answer[(int)(c - 65)]++;
+        }
+        else if ((int)c >= (int)'a')
+        {
+            answer[(int)(c - 71)]++;
+        }
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
