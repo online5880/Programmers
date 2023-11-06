f53a26e91e67b61fe42825a556341c8a1d175906
online5880 <gjtjqkr1@naver.com>
2023-11-07 00:52:45 +0900

    51_배열 만들기 1 52_글자 지우기

---
+++ 51_배열 만들기 1/51_배열 만들기 1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, int k) {
+    vector<int> answer;
+    for (int i = 1; i <= (n / k); ++i)
+    {
+        answer.emplace_back(k * i);
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
---
+++ 52_글자 지우기/52_글자 지우기.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string my_string, vector<int> indices) {
+    string answer = my_string;
+    sort(indices.begin(), indices.end(), greater<>());
+    int prev = 0;
+    for (int n : indices)
+    {
+        answer.erase(n, 1);
+    }
+
+    return answer;
+}
+
+int main()
+{
+    vector<int> a = { 1, 16, 6, 15, 0, 10, 11, 3 };
+    solution("apporoograpemmemprs",a);
+}
+
