3d58f4e6c07faa7df449f04fb97baca9c7a997e3
online5880 <gjtjqkr1@naver.com>
2023-11-07 23:08:52 +0900

    53_카운트 다운

---
+++ 53_카운트 다운/53_카운트 다운.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int start, int end_num) {
+    vector<int> answer;
+    for (int i = start; i >= end_num; --i)
+    {
+        answer.emplace_back(i);
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
