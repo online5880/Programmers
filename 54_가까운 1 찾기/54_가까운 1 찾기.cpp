2ba26942c2655e9a3d07771e824768f9efe88eff
online5880 <gjtjqkr1@naver.com>
2023-11-08 00:14:55 +0900

    54_가까운 1 찾기

---
+++ 54_가까운 1 찾기/54_가까운 1 찾기.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int solution(vector<int> arr, int idx) {
+    int answer = -1;
+
+    if (arr[idx] == 1)
+    {
+        return idx;
+    }
+    else
+    {
+        for (int i = 0; i < arr.size(); ++i)
+        {
+            if (arr[i] == 1 && i > idx)
+            {
+                return i;
+            }
+        }
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
