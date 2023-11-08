92c5163d6a42e8d36ddf1c1bc8eda27eb4c94b34
online5880 <gjtjqkr1@naver.com>
2023-11-09 00:23:10 +0900

    55_리스트 자르기

---
+++ 55_리스트 자르기/55_리스트 자르기.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, vector<int> slicer, vector<int> num_list) {
+    vector<int> answer;
+    if (n == 1)
+    {
+        for (int i = 0; i <= slicer[1]; ++i)
+        {
+            answer.emplace_back(num_list[i]);
+        }
+    }
+    else if (n == 2)
+    {
+        for (int i = slicer[0]; i < num_list.size(); ++i)
+        {
+            answer.emplace_back(num_list[i]);
+        }
+    }
+    else if (n == 3)
+    {
+        for (int i = slicer[0]; i <= slicer[1]; ++i)
+        {
+            answer.emplace_back(num_list[i]);
+        }
+    }
+    else
+    {
+        for (int i = slicer[0]; i <= slicer[1]; i += slicer[2])
+        {
+            answer.emplace_back(num_list[i]);
+        }
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
