dcc0a1fb6459385cae39d16e2bc81f1e0b3e0490
online5880 <gjtjqkr1@naver.com>
2023-11-10 01:00:05 +0900

    57_배열 만들기

---
+++ 57_배열 만들기 3/57_배열 만들기 3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> intervals) {
+    vector<int> answer;
+    for (vector<int> n : intervals)
+    {
+        for (int i = n[0]; i <= n[1]; ++i)
+        {
+            answer.emplace_back(arr[i]);
+        }
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
