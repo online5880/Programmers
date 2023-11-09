9458da874aa54410e00e3f91dfdc3cda65ca2042
online5880 <gjtjqkr1@naver.com>
2023-11-09 22:14:42 +0900

    56_첫 번째로 나오는 음수

---
+++ 56_첫 번째로 나오는 음수/56_첫 번째로 나오는 음수.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = -1;
+    int count = 0;
+    for (int n : num_list)
+    {
+        if (n < 0)
+        {
+            return count;
+        }
+        count++;
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
