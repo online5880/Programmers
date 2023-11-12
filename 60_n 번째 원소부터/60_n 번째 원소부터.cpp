7a0e776011f94bc7674479f862d165cd078aa1c1
online5880 <gjtjqkr1@naver.com>
2023-11-13 00:34:44 +0900

    60_n 번째 원소부터

---
+++ 60_n 번째 원소부터/60_n 번째 원소부터.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list, int n) {
+    vector<int> answer;
+    for (int i = n - 1; i < num_list.size(); ++i)
+    {
+        answer.emplace_back(num_list[i]);
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
