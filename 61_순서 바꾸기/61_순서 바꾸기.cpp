768615acc05049a59d9db662a9646d7496c10541
online5880 <gjtjqkr1@naver.com>
2023-11-13 01:38:46 +0900

    61_순서 바꾸기

---
+++ 61_순서 바꾸기/61_순서 바꾸기.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list, int n) {
+    vector<int> answer;
+    for (int i = n; i < num_list.size(); ++i)
+    {
+        answer.emplace_back(num_list[i]);
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        answer.emplace_back(num_list[i]);
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
