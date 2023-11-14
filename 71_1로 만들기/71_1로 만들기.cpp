47117cddb8a45ac07b446c0f44171bb37d6258fc
online5880 <gjtjqkr1@naver.com>
2023-11-14 22:39:58 +0900

    71_1로 만들기

---
+++ 71_1로 만들기/71_1로 만들기.cpp
@@ -0,0 +1,32 @@
+﻿#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = 0;
+    for (int n : num_list)
+    {
+        int temp = n;
+        while (temp != 1)
+        {
+            if (temp & 1)
+            {
+                temp = (temp - 1) / 2;
+            }
+            else
+            {
+                temp /= 2;
+            }
+            answer++;
+        }
+
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
