524d7e940bfa6a70ec2fa8834b6f9dad2ae0cfe0
online5880 <gjtjqkr1@naver.com>
2023-11-15 00:43:44 +0900

    74_소문자로 바꾸기

---
+++ 74_소문자로 바꾸기/74_소문자로 바꾸기.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string myString) {
+    transform(myString.begin(), myString.end(), myString.begin(), ::tolower);
+    return myString;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
