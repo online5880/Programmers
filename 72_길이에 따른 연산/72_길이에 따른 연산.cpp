a4a3229cc97f03b5212baa29addbe5b765eec606
online5880 <gjtjqkr1@naver.com>
2023-11-14 22:43:56 +0900

    72_길이에 따른 연산

---
+++ 72_길이에 따른 연산/72_길이에 따른 연산.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = 0;
+    if (num_list.size() >= 11)
+    {
+        for (int n : num_list)
+        {
+            answer += n;
+        }
+    }
+    else if (num_list.size() <= 10)
+    {
+        answer = 1;
+        for (int n : num_list)
+        {
+            answer *= n;
+        }
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
