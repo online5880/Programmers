e3b57959931c2e4ee5048ff9495efb2108979d4f
online5880 <gjtjqkr1@naver.com>
2023-11-15 01:20:59 +0900

    77_특정한 문자를 대문자로 바꾸기

---
+++ 77_특정한 문자를 대문자로 바꾸기/77_특정한 문자를 대문자로 바꾸기.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+string solution(string my_string, string alp) {
+    string answer = "";
+    char ch[1000];
+    strcpy(ch, alp.c_str());
+    for (auto& c : my_string)
+    {
+        if (c == alp[0])
+        {
+            c = toupper(c);
+        }
+    }
+    return my_string;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
