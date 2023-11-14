e579e9f26b5bf7196c9a9bccacb0ecdd2ce39e7b
online5880 <gjtjqkr1@naver.com>
2023-11-15 00:55:24 +0900

    75_배열에서 문자열 변환하기

---
+++ 75_배열에서 문자열 대소문자 변환하기/75_배열에서 문자열 대소문자 변환하기.cpp
@@ -0,0 +1,27 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<string> solution(vector<string> strArr) {
+    vector<string> answer;
+    for (int i = 0; i < strArr.size(); ++i)
+    {
+        if (i & 1)
+        {
+            transform(strArr[i].begin(), strArr[i].end(), strArr[i].begin(), ::toupper);
+
+        }
+        else
+        {
+            transform(strArr[i].begin(), strArr[i].end(), strArr[i].begin(), ::tolower);
+        }
+    }
+    return strArr;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
