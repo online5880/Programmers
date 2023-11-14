150db03990a2460091170a01eab7008a0dd72651
online5880 <gjtjqkr1@naver.com>
2023-11-14 21:14:13 +0900

    69_조건에 맞게 수열 반환하기 1

---
+++ 69_조건에 맞게 수열 변환하기 1/69_조건에 맞게 수열 변환하기 1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> answer;
+    for (int n : arr)
+    {
+        if (n & 1 && n < 50)
+        {
+            answer.emplace_back(n * 2);
+            continue;
+        }
+        else if (n % 2 == 0 && n >= 50)
+        {
+            answer.emplace_back(n * 0.5f);
+            continue;
+        }
+        answer.emplace_back(n);
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
