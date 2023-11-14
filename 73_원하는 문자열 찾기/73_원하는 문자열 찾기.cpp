1888779c54fdb316a77710dda00bc75c0d145dce
online5880 <gjtjqkr1@naver.com>
2023-11-14 23:00:00 +0900

    73_원하는 문자열 찾기

---
+++ 73_원하는 문자열 찾기/73_원하는 문자열 찾기.cpp
@@ -0,0 +1,19 @@
+﻿#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int solution(string myString, string pat) {
+    int answer = 0;
+    transform(myString.begin(), myString.end(), myString.begin(), ::tolower);
+    transform(pat.begin(), pat.end(), pat.begin(), ::tolower);
+    if (myString.find(pat) != string::npos)
+    {
+        return 1;
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
