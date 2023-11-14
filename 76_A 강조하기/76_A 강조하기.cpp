8566c079af00a6c40397be2da65ae44cd34c9d7d
online5880 <gjtjqkr1@naver.com>
2023-11-15 01:02:32 +0900

    76_A 강조하기

---
+++ 76_A 강조하기/76_A 강조하기.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    string answer = "";
+    for (char& c : myString)
+    {
+        if (c == 'a')
+        {
+            c = 'A';
+        }
+        else if (c != 'A')
+        {
+            c = tolower(c);
+        }
+    }
+    return myString;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
