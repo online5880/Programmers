09877b5f8c2fff821cecb34a2826cdbfc2dbbc69
online5880 <gjtjqkr1@naver.com>
2023-11-15 00:42:30 +0900

    73_대문자로 바꾸기

---
+++ 73_대문자로 바꾸기/73_대문자로 바꾸기.cpp
@@ -0,0 +1,14 @@
+﻿#include <algorithm>
+#include <iostream>
+using namespace std;
+
+string solution(string myString) {
+    string answer = "";
+    transform(myString.begin(), myString.end(), myString.begin(), ::toupper);
+    return myString;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
