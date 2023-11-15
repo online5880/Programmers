d1f538a4c191fc9f385bc60b9e5a8909f779f787
online5880 <gjtjqkr1@naver.com>
2023-11-15 23:11:26 +0900

    70_조건에 맞게 수열 반환하기 2

--- 70_조건에 맞게 수열 반환하기 2/70_조건에 맞게 수열 반환하기 2.cpp
+++ 70_조건에 맞게 수열 반환하기 2/70_조건에 맞게 수열 반환하기 2.cpp
@@ -1,4 +1,34 @@
 ﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> arr) {
+    int answer = 0;
+    bool result;
+    while (true)
+    {
+        result = false;
+        for (int i = 0; i < arr.size(); ++i)
+        {
+            if (arr[i] & 1 && 50 > arr[i])
+            {
+                arr[i] *= 2;
+                arr[i]++;
+                result = true;
+            }
+            else if (arr[i] % 2 == 0 && arr[i] >= 50)
+            {
+                arr[i] *= 0.5f;
+                result = true;
+            }
+        }
+        if (result == false) break;
+        answer++;
+    }
+    return answer;
+}
 
 int main()
 {
--- 77_특정한 문자를 대문자로 바꾸기/77_특정한 문자를 대문자로 바꾸기.cpp
+++ 77_특정한 문자를 대문자로 바꾸기/77_특정한 문자를 대문자로 바꾸기.cpp
@@ -3,8 +3,6 @@ using namespace std;
 
 string solution(string my_string, string alp) {
     string answer = "";
-    char ch[1000];
-    strcpy(ch, alp.c_str());
     for (auto& c : my_string)
     {
         if (c == alp[0])
