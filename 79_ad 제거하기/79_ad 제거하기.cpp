113ed170b7ed9903a6826487401bf1d91640d24b
online5880 <gjtjqkr1@naver.com>
2023-11-16 23:40:54 +0900

    79_ad 제거하기

---
+++ 79_ad 제거하기/79_ad 제거하기.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(vector<string> strArr) {
+    vector<string> answer;
+    for (int i = 0; i < strArr.size(); ++i)
+    {
+        if (strArr[i].find("ad") == string::npos)
+        {
+            answer.emplace_back(strArr[i]);
+        }
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
