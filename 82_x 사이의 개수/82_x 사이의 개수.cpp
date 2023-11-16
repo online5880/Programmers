839f200f5f5b7840cfa0b7b49a4f37cff4c619e9
online5880 <gjtjqkr1@naver.com>
2023-11-17 01:49:14 +0900

    81_공백으로 구분하기 2 82_x 사이의 개수

---
+++ 82_x 사이의 개수/82_x 사이의 개수.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(string myString) {
+    vector<int> answer;
+    int count = 0;
+    for (auto& c : myString)
+    {
+        if (c == 'x')
+        {
+            answer.emplace_back(count);
+            count = 0;
+        }
+        else
+        {
+            count++;
+        }
+    }
+    answer.emplace_back(count);
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
