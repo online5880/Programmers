176424e95820c3d7ea7d4149d944365b8ccea456
online5880 <gjtjqkr1@naver.com>
2023-11-16 21:03:20 +0900

    78_문자열이 몇 번등장하는지 세기

---
+++ 78_문자열이 몇 번 등장하는지 세기/78_문자열이 몇 번 등장하는지 세기.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int solution(string myString, string pat) {
+    int answer = 0;
+    for (int i = 0; i < myString.length(); ++i)
+    {
+        if (myString.substr(i, pat.size()) == pat)
+        {
+            ++answer;
+        }
+    }
+    return answer;
+
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
