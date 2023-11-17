72b30a73f43e759d77ae6083b762f7ae5f17691d
online5880 <gjtjqkr1@naver.com>
2023-11-18 00:10:35 +0900

    84_세 개의 구분자

---
+++ 84_세 개의 구분자/84_세 개의 구분자.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(string myStr) {
+    vector<string> answer;
+    for (auto& c : myStr)
+    {
+        if (c == 'a' || c == 'b' || c == 'c')
+        {
+            c = ' ';
+        }
+    }
+
+    stringstream ss(myStr);
+    string str;
+    while (ss >> str)
+    {
+        answer.emplace_back(str);
+    }
+    if (answer.empty())
+    {
+        answer.emplace_back("EMPTY");
+    }
+
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
