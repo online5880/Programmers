e5b1f49cbd214125e7ef21a008d8fb6c363c5d09
online5880 <gjtjqkr1@naver.com>
2023-11-18 00:15:34 +0900

    85_배열의 원소만큼 추가하기

---
+++ 85_배열의 원소만큼 추가하기/85_배열의 원소만큼 추가하기.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> answer;
+    for (int n : arr)
+    {
+        for (int i = 0; i < n; ++i)
+        {
+            answer.emplace_back(n);
+        }
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
