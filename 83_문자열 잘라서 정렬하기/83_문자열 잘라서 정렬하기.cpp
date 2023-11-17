0b1013edc4094d27c5748ad97b0c22b6f8ad4aa9
online5880 <gjtjqkr1@naver.com>
2023-11-17 21:20:43 +0900

    83_문자열 잘라서 정렬하기

---
+++ 83_문자열 잘라서 정렬하기/83_문자열 잘라서 정렬하기.cpp
@@ -0,0 +1,31 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(string myString) {
+    vector<string> answer;
+    for (auto& c : myString)
+    {
+        if (c == 'x')
+        {
+            c = ' ';
+        }
+    }
+    stringstream ss(myString);
+    string str;
+    while (ss >> str)
+    {
+        answer.emplace_back(str);
+    }
+    sort(answer.begin(), answer.end());
+
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
