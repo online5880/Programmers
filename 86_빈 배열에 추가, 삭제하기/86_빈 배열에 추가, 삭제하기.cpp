da053dedca06399c3e73522b659aa3cdf48b93d9
online5880 <gjtjqkr1@naver.com>
2023-11-18 00:30:31 +0900

    86_빈 배열에 추가, 삭제하기

---
+++ 86_빈 배열에 추가, 삭제하기/86_빈 배열에 추가, 삭제하기.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<bool> flag) {
+    vector<int> answer;
+    for (int i = 0; i < arr.size(); ++i)
+    {
+        if (flag[i])
+        {
+            for (int j = 0; j < arr[i] * 2; ++j)
+            {
+
+                answer.emplace_back(arr[i]);
+            }
+        }
+        else
+        {
+            for (int j = 0; j < arr[i]; ++j)
+            {
+                answer.pop_back();
+            }
+        }
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
