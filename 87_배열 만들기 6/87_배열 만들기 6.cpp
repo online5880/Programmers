400003b35bad458b7fe32fc1716e0ef3b073e8ea
online5880 <gjtjqkr1@naver.com>
2023-11-20 00:16:12 +0900

    87_배열 만들기 6

--- 87_배열 만들기 6/87_배열 만들기 6.cpp
+++ 87_배열 만들기 6/87_배열 만들기 6.cpp
@@ -7,15 +7,26 @@ using namespace std;
 vector<int> solution(vector<int> arr) {
     vector<int> answer;
     int i = 0;
-    while (i < arr.size()) {
-        if (answer.empty()) answer.emplace_back(arr[i++]);
-        else if (answer.back() != arr[i]) answer.emplace_back(arr[i++]);
-        else {
+    while (i < arr.size())
+    {
+        if (answer.empty())
+        {
+            answer.emplace_back(arr[i++]);
+        }
+        else if (answer.back() != arr[i])
+        {
+            answer.emplace_back(arr[i++]);
+        }
+        else
+        {
             answer.pop_back();
             i++;
         }
     }
-    if (answer.empty()) answer.emplace_back(-1);
+    if (answer.empty())
+    {
+        answer.emplace_back(-1);
+    }
     return answer;
 }
 
