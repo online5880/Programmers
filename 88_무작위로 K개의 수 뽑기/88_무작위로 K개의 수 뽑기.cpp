b0985a3776f8b9bcedbc9095fb2111ea935b7a9c
online5880 <gjtjqkr1@naver.com>
2023-11-20 00:44:45 +0900

    88_무작위로 K개의 수 뽑기

---
+++ 88_무작위로 K개의 수 뽑기/88_무작위로 K개의 수 뽑기.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, int k) {
+    vector<int> answer;
+    unordered_set<int> us;
+    for (int n : arr)
+    {
+        if (us.find(n) == us.end())
+        {
+            us.insert(n);
+            answer.emplace_back(n);
+        }
+        if (answer.size() == k)
+        {
+            break;
+        }
+    }
+    while (answer.size() < k)
+    {
+        answer.emplace_back(-1);
+    }
+
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
