eb36c788856b7e9c2c01682c8b270a33d76dace4
online5880 <gjtjqkr1@naver.com>
2023-11-21 01:00:40 +0900

    08_문자열 내 p와 y의 개수

---
+++ 08_문자열 내 p와 y의 개수/08_문자열 내 p와 y의 개수.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    int p = 0;
+    int y = 0;
+
+    for (auto c : s)
+    {
+        if (c == 'P' || c == 'p')
+        {
+            p++;
+        }
+        if (c == 'Y' || c == 'y')
+        {
+            y++;
+        }
+    }
+    cout << "Hello Cpp" << endl;
+
+    return p == y ? true : false;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
