7912bc8979be0dc3436469d6c56586cca2a8d610
online5880 <gjtjqkr1@naver.com>
2023-11-20 21:10:54 +0900

    05_x만큼 간격이 있는 n개의 숫자

---
+++ 05_x만큼 간격이 있는 n개의 숫자/05_x만큼 간격이 있는 n개의 숫자.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<long long> solution(int x, int n) {
+    vector<long long> answer;
+    for (int i = 1; i <= n; ++i)
+    {
+        answer.emplace_back(i * x);
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
