bb6419861a65c739d1b2108010e81292319841c0
online5880 <gjtjqkr1@naver.com>
2023-11-21 01:05:23 +0900

    09_자릿수 더하기

---
+++ 09_자릿수 더하기/09_자릿수 더하기.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+int solution(int n)
+{
+    int answer = 0;
+    string number = to_string(n);
+    for (auto n : number)
+    {
+        int temp = (int)n - 48;
+        answer += temp;
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
