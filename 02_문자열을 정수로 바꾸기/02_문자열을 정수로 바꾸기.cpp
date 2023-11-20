56a17c6ddcd06c162513dfa892b86ecfbcd0e503
online5880 <gjtjqkr1@naver.com>
2023-11-20 21:01:18 +0900

    02_문자열을 정수로 바꾸기

---
+++ 02_문자열을 정수로 바꾸기/02_문자열을 정수로 바꾸기.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 1;
+    while (true)
+    {
+        if (n % answer == 1)
+        {
+            break;
+        }
+        answer++;
+    }
+    return answer;
+}
+int main()
+{
+    std::cout << "Hello World!\n";
+}
