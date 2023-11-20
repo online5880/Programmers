e8450474c8e2c6a9ec9c572010687cc6eab578d7
online5880 <gjtjqkr1@naver.com>
2023-11-20 21:13:02 +0900

    06_평균 구하기

---
+++ 06_평균 구하기/06_평균 구하기.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    double answer = 0;
+    for (int n : arr)
+    {
+        answer += n;
+    }
+    return answer / arr.size();
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
