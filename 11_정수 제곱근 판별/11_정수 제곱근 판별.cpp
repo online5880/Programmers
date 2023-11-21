8a6f433834184c14fde75c13540f10ba2a84c581
online5880 <gjtjqkr1@naver.com>
2023-11-22 00:06:43 +0900

    11_정수 제곱근 판별

---
+++ 11_정수 제곱근 판별/11_정수 제곱근 판별.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+long long solution(long long n) {
+    long long answer = 0;
+    for (long long i = 1; i <= n; ++i)
+    {
+        if (pow(i, 2) == n)
+        {
+            return pow(i + 1, 2);
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
