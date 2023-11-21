d8806b494a02343737f0f828d45dfaef18755187
online5880 <gjtjqkr1@naver.com>
2023-11-21 20:22:00 +0900

    10_자연수 뒤집어 배열로 만들기

---
+++ 10_자연수 뒤집어 배열로 만들기/10_자연수 뒤집어 배열로 만들기.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<int> solution(long long n) {
+    vector<int> answer;
+    string str = to_string(n);
+    reverse(str.begin(), str.end());
+    for (int i = 0; i < str.length(); ++i)
+    {
+        answer.emplace_back(str[i] - 48);
+    }
+
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
