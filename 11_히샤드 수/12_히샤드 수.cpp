a5ae585216c0bea51e158a7514318381faf85ab3
online5880 <gjtjqkr1@naver.com>
2023-11-23 00:03:17 +0900

    12_히샤드 수

---
+++ 11_히샤드 수/12_히샤드 수.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+bool solution(int x) {
+    bool answer = true;
+    string str = to_string(x);
+    int temp = 0;
+    for (int i = 0; i < str.length(); ++i)
+    {
+        int n = str[i] - 48;
+        temp += n;
+    }
+    return x % temp == 0;
+}
+int main()
+{
+    std::cout << "Hello World!\n";
+}
