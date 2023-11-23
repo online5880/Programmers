d85008570c382b9e08017e2a1fae266582a5c0f5
online5880 <gjtjqkr1@naver.com>
2023-11-23 20:08:35 +0900

    13_두 정수 사이의 합

---
+++ 13_두 정수 사이의 합/13_두 정수 사이의 합.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int a, int b) {
+    long long answer = 0;
+    if (a < b)
+    {
+        for (int i = a; i <= b; ++i)
+        {
+            answer += i;
+        }
+    }
+    else
+    {
+        for (int i = b; i <= a; ++i)
+        {
+            answer += i;
+        }
+    }
+
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
