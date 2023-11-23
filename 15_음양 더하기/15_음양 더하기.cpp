8f2c67f862ad1650eab16e142bbba35f3285a363
online5880 <gjtjqkr1@naver.com>
2023-11-24 00:34:58 +0900

    15_음양 더하기

---
+++ 15_음양 더하기/15_음양 더하기.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int answer = 0;
+    for (int i = 0; i < absolutes.size(); ++i)
+    {
+        if (!signs[i])
+        {
+            absolutes[i] *= -1;
+        }
+    }
+    for (int n : absolutes)
+    {
+        answer += n;
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
