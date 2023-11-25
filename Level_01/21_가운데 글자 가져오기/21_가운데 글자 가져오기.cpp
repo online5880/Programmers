cfcba22fcdf99d86530800acd922e5480cbbdd33
online5880 <gjtjqkr1@naver.com>
2023-11-26 01:08:31 +0900

    21_가운데 글자 가져오기

---
+++ Level_01/21_가운데 글자 가져오기/21_가운데 글자 가져오기.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    if (s.length() % 2 == 1)
+    {
+        answer = s[s.length() / 2];
+    }
+    else
+    {
+        answer = s[s.length() / 2 - 1];
+        answer += s[s.length() / 2];
+    }
+
+    return answer;
+}
+// 다른 사람 풀이
+/*string solution(string s) {
+    return s.length() & 1 ? s.substr(s.length() * 0.5, 1) : s.substr(s.length() * 0.5 - 1, 2);
+}*/
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
