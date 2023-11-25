eabc28bf212e4ca59aa1a037bc5cda1c0f876a0e
online5880 <gjtjqkr1@naver.com>
2023-11-25 17:52:00 +0900

    17_나누어 떨어지는 숫자 배열

---
+++ 17_나누어 떨어지는 숫자 배열/17_나누어 떨어지는 숫자 배열.cpp
@@ -0,0 +1,25 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, int divisor) {
+    vector<int> answer;
+    for (int n : arr)
+    {
+        if (n % divisor == 0 || n / divisor == n)
+        {
+            answer.emplace_back(n);
+        }
+    }
+    sort(answer.begin(), answer.end());
+    if (answer.empty()) { answer.emplace_back(-1); }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
