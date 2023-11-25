7fd87e587e68e2cca131ce05127405a5429d8294
online5880 <gjtjqkr1@naver.com>
2023-11-25 18:20:30 +0900

    19_핸드폰 번호 가리기

---
+++ 19_핸드폰 번호 가리기/19_핸드폰 번호 가리기.cpp
@@ -0,0 +1,22 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <regex>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string phone_number) {
+    string answer = "";
+    int size = phone_number.length() - 4;
+    for (int i = 0; i < size; ++i)
+    {
+        phone_number[i] = '*';
+    }
+    return phone_number;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
