10b90e56379ca96a14587831eece60108836246a
online5880 <gjtjqkr1@naver.com>
2023-11-26 00:44:49 +0900

    20_제일 작은 수 제거하기

---
+++ 20_제일 작은 수 제거하기/20_제일 작은 수 제거하기.cpp
@@ -0,0 +1,35 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> answer;
+
+    if (arr.size() == 1)
+    {
+        answer.emplace_back(-1);
+        return answer;
+    }
+
+    auto minElement = min_element(arr.begin(), arr.end());
+    int index = distance(arr.begin(), minElement);
+    arr.erase(arr.begin() + index);
+    answer = arr;
+    return answer;
+}
+
+// 다른 사람 풀이
+/*vector<int> solution(vector<int> arr) {
+    if (arr.size() == 1) {
+        return { -1 };
+    }
+    arr.erase(std::min_element(arr.begin(), arr.end()));
+    return arr;
+}*/
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
