b7c9b52d524ea006d412b8f034391f19b3c8f948
online5880 <gjtjqkr1@naver.com>
2023-11-26 01:17:55 +0900

    22_수박수박수박수박수박수?

---
+++ 22_수박수박수박수박수박수/22_수박수박수박수박수박수.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "수";
+    for (int i = 1; i < n; ++i)
+    {
+        if (i & 1)
+        {
+            answer += "박";
+        }
+        else
+        {
+            answer += "수";
+        }
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
