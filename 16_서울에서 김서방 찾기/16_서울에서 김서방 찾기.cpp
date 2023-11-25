b6e708d6db23296acafd3f0bd478a158aff55e25
online5880 <gjtjqkr1@naver.com>
2023-11-25 17:46:30 +0900

    16_서울에서 김서방 찾기

---
+++ 16_서울에서 김서방 찾기/16_서울에서 김서방 찾기.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> seoul) {
+    string answer = "김서방은 ";
+    int index = 0;
+    for (int i = 0; i < seoul.size(); ++i)
+    {
+        if (seoul[i] == "Kim")
+        {
+            return "김서방은 " + to_string(i) + "에 있다";
+        }
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
