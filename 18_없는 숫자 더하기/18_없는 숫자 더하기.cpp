7b7138f984b3150b4b82d8ecbb5da92f23965041
online5880 <gjtjqkr1@naver.com>
2023-11-25 18:01:52 +0900

    18_없는 숫자 더하기

---
+++ 18_없는 숫자 더하기/18_없는 숫자 더하기.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+    int answer = 0;
+    for (int i = 0; i < 10; ++i)
+    {
+        auto number = find(numbers.begin(), numbers.end(), i);
+        if (number == numbers.end())
+        {
+            answer += i;
+        }
+    }
+    return answer;
+}
+
+// 다른 사람 풀이
+// 총합에서 뺀다.
+
+/*int solution(vector<int> numbers) {
+    return 45 - accumulate(numbers.begin(), numbers.end(), 0);
+}*/
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
