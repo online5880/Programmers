d41f635ef8013c737c671d7b9012ec044d655823
online5880 <gjtjqkr1@naver.com>
2023-11-27 00:04:29 +0900

    24_약수의 개수와 덧셈

---
+++ Level_01/24_약수의 개수와 덧셈/24_약수의 개수와 덧셈.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int solution(int left, int right) {
+    int answer = 0;
+    for (int i = left; i <= right; ++i)
+    {
+        int temp = 0;
+        for (int j = 1; j <= i; ++j)
+        {
+            if (i % j == 0)
+            {
+                temp++;
+            }
+        }
+        answer = temp & 1 ? answer -= i : answer += i;
+    }
+
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
