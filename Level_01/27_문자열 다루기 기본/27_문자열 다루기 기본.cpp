a1ac73c5ba433381208864cb11683fd7ec339d11
online5880 <gjtjqkr1@naver.com>
2023-11-27 00:15:22 +0900

    27_문자열 다루기 기본

---
+++ Level_01/27_문자열 다루기 기본/27_문자열 다루기 기본.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <regex>
+using namespace std;
+
+bool solution(string s) {
+	const regex pattern("^(\\d{4}|\\d{6})$");
+    return regex_match(s, pattern);
+}
+
+// 다른 사람 풀이
+/*bool solution(string s) {
+    bool answer = true;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (!isdigit(s[i]))
+            answer = false;
+    }
+
+    return s.size() == 4 || s.size() == 6 ? answer : false;
+}*/
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
