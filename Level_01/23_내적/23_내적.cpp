a37d8aca829556c5ffb6cf54d6e5d2fecc43217b
online5880 <gjtjqkr1@naver.com>
2023-11-26 13:10:39 +0900

    23_내적

---
+++ Level_01/23_내적/23_내적.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <numeric>
+#include <vector>
+using namespace std;
+
+int solution(vector<int> a, vector<int> b) {
+    int answer = 1234567890;
+    answer = inner_product(a.begin(), a.end(), b.begin(), 0);
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
