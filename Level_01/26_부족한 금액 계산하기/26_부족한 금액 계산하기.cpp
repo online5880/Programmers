3dbd69a3408b408c5343e7526d39b99454c9e5a1
online5880 <gjtjqkr1@naver.com>
2023-11-27 00:06:33 +0900

    26_부족한 금액 계산하기

---
+++ Level_01/26_부족한 금액 계산하기/26_부족한 금액 계산하기.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+long long solution(int price, int money, int count)
+{
+    long long answer = 0;
+    for (int i = 1; i <= count; ++i)
+    {
+        answer += (price * i);
+    }
+    if (answer > money)
+        answer -= money;
+    else
+    {
+        answer = 0;
+    }
+
+    return answer;
+}
+
+// 다른 사람 풀이
+/*typedef long long ll;
+
+long long solution(int price, int money, int count)
+{
+    ll answer = (ll)(count * (count + 1) / 2) * price;
+    return answer > money ? answer - money : 0;
+}*/
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
