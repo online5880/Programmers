f0be0e90c1782cb92ddd1e2f6f1cb5bda3b88ea8
online5880 <gjtjqkr1@naver.com>
2023-11-27 01:15:03 +0900

    28_직사각형 별찍기

---
+++ 28_직사각형 별찍기/28_직사각형 별찍기.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+/*int main(void) {
+    int a;
+    int b;
+    cin >> a >> b;
+    for (int i = 0; i < b; ++i)
+    {
+        for (int j = 0; j < a; ++j)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}*/
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
