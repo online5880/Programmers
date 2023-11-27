a9bbd5482347faa75928da1c7aeffdc5d5986c75
online5880 <gjtjqkr1@naver.com>
2023-11-28 01:06:02 +0900

    30_같은 숫자는 싫어

---
+++ Level_01/30_같은 숫자는 싫어/30_같은 숫자는 싫어.cpp
@@ -0,0 +1,21 @@
+﻿#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr)
+{
+    vector<int> answer;
+    answer.emplace_back(arr[0]);
+    for (int i = 0; i < arr.size(); ++i)
+    {
+        if (answer.back() != arr[i])
+            answer.emplace_back(arr[i]);
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
