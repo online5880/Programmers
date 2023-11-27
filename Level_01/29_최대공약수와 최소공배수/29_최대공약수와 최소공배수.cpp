141a7c0d22e0405030f889d0be435596e1b4267a
online5880 <gjtjqkr1@naver.com>
2023-11-27 22:20:10 +0900

    29_최대공약수와 최소공배수

---
+++ Level_01/29_최대공약수와 최소공배수/29_최대공약수와 최소공배수.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int gdc(int a, int b)
+{
+    if (b == 0) return a;
+    return gdc(b, a % b);
+}
+
+int lcm(int a, int b)
+{
+    return a * b / gdc(a, b);
+}
+
+vector<int> solution(int n, int m) {
+    vector<int> answer;
+    answer.emplace_back(gdc(n, m));
+    answer.emplace_back(lcm(n, m));
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
