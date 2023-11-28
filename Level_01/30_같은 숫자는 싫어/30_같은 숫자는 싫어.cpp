978ac17719e14ef482ca72ac65d961e9c4b17739
online5880 <gjtjqkr1@naver.com>
2023-11-28 21:27:55 +0900

    31_3진법 뒤집기

--- Level_01/30_같은 숫자는 싫어/30_같은 숫자는 싫어.cpp
+++ Level_01/30_같은 숫자는 싫어/30_같은 숫자는 싫어.cpp
@@ -7,10 +7,10 @@ vector<int> solution(vector<int> arr)
 {
     vector<int> answer;
     answer.emplace_back(arr[0]);
-    for (int i = 0; i < arr.size(); ++i)
+    for (int& i : arr)
     {
-        if (answer.back() != arr[i])
-            answer.emplace_back(arr[i]);
+        if (answer.back() != i)
+            answer.emplace_back(i);
     }
     return answer;
 }
---
+++ Level_01/31_3진법 뒤집기/31_3진법 뒤집기.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    vector<int> three;
+    int temp = n;
+    while (temp)
+    {
+        three.emplace_back(temp % 3);
+        temp /= 3;
+    }
+    reverse(three.begin(), three.end());
+    int m = 1;
+    for (int i : three)
+    {
+        answer += (i * m);
+        m *= 3;
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
