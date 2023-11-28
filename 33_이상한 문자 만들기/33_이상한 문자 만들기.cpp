ae25b6426650cf8c79e74fc18a8e88faeccb84a2
online5880 <gjtjqkr1@naver.com>
2023-11-28 22:02:09 +0900

    33_이상한 문자 만들기

---
+++ 33_이상한 문자 만들기/33_이상한 문자 만들기.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    int index = 0;
+    for (char& i : s)
+    {
+        if (i == ' ')
+        {
+            index = 0;
+            continue;
+        }
+        i = index & 1 ? (char)tolower(i) : (char)toupper(i);
+        index++;
+    }
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
