84fa4692605f0a2016cc1fe32fa1ff72a27c4418
online5880 <gjtjqkr1@naver.com>
2023-11-28 21:45:52 +0900

    32_예산

---
+++ Level_01/32_예산/32_예산.cpp
@@ -0,0 +1,25 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int answer = 0;
+    sort(d.begin(), d.end());
+    for (const int i : d)
+    {
+        if (budget < i)
+        {
+            break;
+        }
+        budget -= i;
+        answer++;
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
