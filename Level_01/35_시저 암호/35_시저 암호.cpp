5509db169ffcf5ad1182ae458c227a631af8e6ec
online5880 <gjtjqkr1@naver.com>
2023-11-30 23:54:19 +0900

    35_시저 암호

---
+++ Level_01/35_시저 암호/35_시저 암호.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s, int n) {
+    string answer = "";
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] != ' ')
+        {
+            if (s[i] >= 'a' && s[i] <= 'z')
+                answer += (s[i] - 'a' + n) % 26 + 'a';
+            else
+                answer += (s[i] - 'A' + n) % 26 + 'A';
+        }
+        else
+            answer += ' ';
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
