df1b277235c3054ffb7064500e9da6db30deebb9
online5880 <gjtjqkr1@naver.com>
2023-12-03 23:17:41 +0900

    38_삼총사

---
+++ Level_01/38_삼총사/38_삼총사.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> number) {
+    int answer = 0;
+    sort(number.begin(), number.end());
+    for (int i = 0; i < number.size() - 2; ++i)
+    {
+        for (int j = i + 1; j < number.size() - 1; ++j)
+        {
+            for (int k = j + 1; k < number.size(); ++k)
+            {
+                if (number[i] + number[j] + number[k] == 0)
+                {
+                    answer++;
+                }
+            }
+        }
+    }
+    return answer;
+}
+// 투포인터 알고리즘으로 풀 수 있다.
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
