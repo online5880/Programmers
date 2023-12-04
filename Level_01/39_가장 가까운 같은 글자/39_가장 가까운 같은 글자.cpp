75d054e26b42c95f2615f714938e3177917094f2
online5880 <gjtjqkr1@naver.com>
2023-12-04 20:23:30 +0900

    39_가장 까가운 같은 글자

---
+++ Level_01/39_가장 가까운 같은 글자/39_가장 가까운 같은 글자.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(string s) {
+    vector<int> answer;
+    for (int i = 0; i < s.length(); ++i)
+    {
+        int index = -1;
+        for (int j = 0; j < i; ++j) {
+            if (s[i] == s[j]) {
+                index = i - j;
+            }
+        }
+        answer.emplace_back(index);
+    }
+
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
