14f23148cbc4dcbcb6efe08e181c2279c7cd6020
online5880 <gjtjqkr1@naver.com>
2023-12-06 01:19:33 +0900

    40_K번째수

---
+++ Level_01/40_K번쨰수/40_K번쨰수.cpp
@@ -0,0 +1,29 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    for (auto command : commands)
+    {
+        vector<int> temp;
+        int index = 0;
+        for (int i = command[0] - 1; i < command[1]; ++i)
+        {
+            temp.emplace_back(array[i]);
+        }
+        sort(temp.begin(), temp.end());
+        index = temp[command[2] - 1];
+        answer.emplace_back(index);
+        index = 0;
+    }
+
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
