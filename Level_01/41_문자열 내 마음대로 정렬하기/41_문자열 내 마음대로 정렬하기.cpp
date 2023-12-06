9a27444ffbffa2eaeca657cea5cdc474b8d78970
online5880 <gjtjqkr1@naver.com>
2023-12-06 22:23:42 +0900

    41_문자열 내 마음대로 정렬하기

---
+++ Level_01/41_문자열 내 마음대로 정렬하기/41_문자열 내 마음대로 정렬하기.cpp
@@ -0,0 +1,33 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+int INDEX;
+
+bool cmp(string a, string b)
+{
+    if (a[INDEX] == b[INDEX])
+    {
+        return a < b;
+    }
+    else
+    {
+        return a[INDEX] < b[INDEX];
+    }
+}
+
+vector<string> solution(vector<string> strings, int n) {
+    vector<string> answer;
+    INDEX = n;
+    sort(strings.begin(), strings.end(), cmp);
+    return strings;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
