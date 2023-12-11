c712d5984cce103a5ab9d8e57c5e7ac39a1fbe3e
online5880 <gjtjqkr1@naver.com>
2023-12-11 21:27:49 +0900

    42_콜라 문제

---
+++ 42_콜라 문제/42_콜라 문제.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b, int n) {
+    int answer = 0;
+
+    while (n >= a)
+    {
+	    const int bottle = n / a * b;
+        n %= a;
+        answer += bottle;
+
+        n += bottle;
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
