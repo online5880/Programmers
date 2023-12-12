fa7d9e90ee63c396f876be5eb64e153f6ea0d0be
online5880 <gjtjqkr1@naver.com>
2023-12-12 21:04:39 +0900

    43_2016년

---
+++ Level_01/43_2016년/43_2016년.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int a, int b) {
+    int month[] = { 31,29,31,30,31,30,31,31,30,31,30,31 };
+    string day[] = { "THU","FRI","SAT","SUN","MON","TUE","WED" };
+    int today = b;
+    for (int i = 0; i < a - 1; ++i)
+    {
+        today += month[i];
+    }
+    return day[today % 7];
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
