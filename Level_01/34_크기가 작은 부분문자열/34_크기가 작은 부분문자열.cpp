415dca5867a229c8e0585125bd14865db3c7d1f8
online5880 <gjtjqkr1@naver.com>
2023-12-13 21:15:55 +0900

    44_추억 점수

--- Level_01/34_크기가 작은 부분문자열/34_크기가 작은 부분문자열.cpp
+++ Level_01/34_크기가 작은 부분문자열/34_크기가 작은 부분문자열.cpp
@@ -6,13 +6,15 @@ using namespace std;
 
 int solution(string t, string p) {
     int answer = 0;
-    int t_length = t.length();
-    int p_length = p.length();
-    long long num = stoll(p);
+    long long t_length(t.length());
+    long long p_length(p.length());
+    long long length(stoll(p));
     for (int i = 0; i <= (t_length - p_length); ++i)
     {
-        if (stoll(t.substr(i, p_length)) <= num)
+        if (stoll(t.substr(i, p_length)) <= length)
+        {
             answer++;
+        }
     }
     return answer;
 }
---
+++ Level_01/44_추억 점수/44_추억 점수.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
+    vector<int> answer;
+    unordered_map<string, int> score;
+
+    for (int i = 0; i < name.size(); ++i)
+    {
+        score.emplace(name[i], yearning[i]);
+    }
+
+    for (const auto& p : photo)
+    {
+        int temp = 0;
+        int count = 0;
+        for (const auto& s : p)
+        {
+            if (auto search = score.find(s) != score.end())
+            {
+                temp += score[s];
+            }
+        }
+        answer.emplace_back(temp);
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
