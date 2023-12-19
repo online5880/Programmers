d96880ffbf7dfb8e6dfee318daf4771b5eee159f
online5880 <gjtjqkr1@naver.com>
2023-12-19 20:33:17 +0900

    47_푸드 파이트 대회

---
+++ 47_푸드 파이트 대회/47_푸드 파이트 대회.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+string solution(vector<int> food) {
+    string answer = "";
+    for (int i = 1; i < food.size(); ++i)
+    {
+        for (int j = 0; j < food[i] / 2; ++j)
+        {
+            answer += to_string(i);
+        }
+    }
+    string temp = answer;
+    reverse(temp.begin(), temp.end());
+    return answer + '0' + temp;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
