35c78d670942e2ffd485f4608f6daabd37e5d147
online5880 <gjtjqkr1@naver.com>
2023-12-28 20:54:07 +0900

    48_폰켓몬

---
+++ Level_01/48_폰켓몬/48_폰켓몬.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <set>
+#include <vector>
+using namespace std;
+
+int solution(vector<int> nums)
+{
+    set<int> unique_ponkemon;
+    for (int n : nums)
+    {
+        unique_ponkemon.emplace(n);
+    }
+
+    return min(unique_ponkemon.size(), nums.size() / 2);
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
