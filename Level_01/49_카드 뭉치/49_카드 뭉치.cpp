e0ac1c1cdc46097ccc32e9f41a3425124dbfcc26
online5880 <gjtjqkr1@naver.com>
2024-01-02 20:16:57 +0900

    49_카드 뭉치

---
+++ Level_01/49_카드 뭉치/49_카드 뭉치.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
+    string answer = "";
+    int j = 0, k = 0;
+    for (int i = 0; i < goal.size(); ++i)
+    {
+        if (goal[i] == cards1[j])
+        {
+            j++;
+            continue;
+        }
+        else if (goal[i] == cards2[k])
+        {
+            k++;
+            continue;
+        }
+        else
+        {
+            answer = "No";
+            return answer;
+        }
+    }
+    answer = "Yes";
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
